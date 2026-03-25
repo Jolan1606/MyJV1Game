c5d3da96b4813ea27480535b23b4fffc75a0f8e7
Jolan1606 <132004366+Jolan1606@users.noreply.github.com>
2026-03-25 02:33:01 +0100

    ça marche emerge

--- Assets/Script/Entity/SandWorm/Emerge.cs
+++ Assets/Script/Entity/SandWorm/Emerge.cs
@@ -1,29 +1,61 @@
 using UnityEngine;
+using System.Collections;
 
 public class Emerge : MonoBehaviour
 {
     public GameObject SD;
-   
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-     
-    }
+    private bool alreadyTrigger; 
+    public float emergeDuration = 1.0f; // Durée de l’émergence en secondes 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.CompareTag("Player") == true)
         {
-            Debug.Log("cadetecte");
 
-            SD.transform.position = transform.position = new Vector3(transform.position.x, transform.position.y + (float)1.5 , transform.position.z);
-
+            if (alreadyTrigger == true)
+            {
+                return;
+            }
+            else
+            {
+                Debug.Log("cadetecte");
+                StartCoroutine(SmoothEmerge());
+                //SD.transform.position = transform.position = new Vector3(transform.position.x, transform.position.y + (float)1.5, transform.position.z);
+                alreadyTrigger = true;
+            }
         }
+    }
+    private IEnumerator SmoothEmerge()
+    {
+        if (SD == null) yield break;
+
+        // Position de départ 
+        Vector3 startPos = SD.transform.position;
+
+        // Position d'arrivée
+        Vector3 targetPos = new Vector3(
+            transform.position.x,
+            transform.position.y + 1.5f,
+            transform.position.z
+        );
+
+        float elapsed = 0f;
+
+        while (elapsed < emergeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / emergeDuration;           
+
+            // Interpolation linéaire 
+            SD.transform.position = Vector3.Lerp(startPos, targetPos, t);
+
+            yield return null;   //  attend la prochaine frame
+        }
+
+        //  s’assure qu’il arrive pile à la fin
+        SD.transform.position = targetPos;
     }
 }
