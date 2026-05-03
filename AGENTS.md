6db49fb9632f8240d11a818f7a0bf2896011890f
Jolan1606 <132004366+Jolan1606@users.noreply.github.com>
2026-05-03 23:19:21 +0200

    c'est plus tout cassé !!!!

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: MyJV1Game
 - Unity version: Unity 6000.3.6f1
 - Active game object:
-  - Name: Menu Canvas
-  - Tag: Untagged
-  - Layer: UI
+  - Name: player
+  - Tag: Player
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Script/player_Script/PlayerAttack.cs
+++ Assets/Script/player_Script/PlayerAttack.cs
@@ -2,47 +2,46 @@
 
 public class PlayerAttack : MonoBehaviour
 {
-    public SandHealth SandHealth; // Référence à la classe de santé de l'ennemi
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-       if (SandHealth == null)
-        {
-            SandHealth = Object.FindAnyObjectByType<SandHealth>();
-            if (SandHealth == null)
-                Debug.LogError("SandHealth introuvable ! Vérifie que ton Ennemy est bien dans la scène.");
-        }
-    }
+    private SandHealth currentTarget;   
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+       
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Debug.Log("Attaque déclenchée !");
+
+            if (currentTarget != null)
+            {
+                Debug.Log("Attaque réussie sur l'ennemi !");
+                currentTarget.TakeDamage(20f);
+            }
+            else
+            {
+                Debug.Log("Aucun ennemi à portée...");
+            }
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-
-
         if (collision.CompareTag("Ennemy"))
         {
-            Debug.Log("Ennemi détecté !");
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-              
-                
-                    Debug.Log("Attaque réussie !");
-                    SandHealth.TakeDamage(100f );
+            
+            currentTarget = collision.GetComponent<SandHealth>();
 
-             
+            if (currentTarget != null)
+            {
+                Debug.Log("Ennemi détecté et cible verrouillée !");
             }
         }
-      
     }
 
-
-
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ennemy"))
+        {
+            currentTarget = null;
+        }
+    }
 }
